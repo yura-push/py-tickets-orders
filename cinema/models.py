a74a309edfdff062edc797cde220700eca00efac
Yura Push <youdeimos1@gmail.com>
2025-02-04 13:06:15 +0200

    minor changes

--- cinema/models.py
+++ cinema/models.py
@@ -84,6 +84,14 @@ class Ticket(models.Model):
     row = models.IntegerField()
     seat = models.IntegerField()
 
+    def __str__(self):
+        return (
+            f"{str(self.movie_session)} (row: {self.row}, seat: {self.seat})"
+        )
+
+    class Meta:
+        unique_together = ("movie_session", "row", "seat")
+
     def clean(self):
         for ticket_attr_value, ticket_attr_name, cinema_hall_attr_name in [
             (self.row, "row", "rows"),
@@ -113,11 +121,3 @@ class Ticket(models.Model):
         super(Ticket, self).save(
             force_insert, force_update, using, update_fields
         )
-
-    def __str__(self):
-        return (
-            f"{str(self.movie_session)} (row: {self.row}, seat: {self.seat})"
-        )
-
-    class Meta:
-        unique_together = ("movie_session", "row", "seat")
